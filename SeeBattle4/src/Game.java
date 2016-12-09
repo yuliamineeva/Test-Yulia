85967dd0d257d8cc408f58eb69e1482295729759
yuliamineeva <yuliamineeva@yandex.ru>
2016-12-09 11:30:16 +0500

    change class Game

--- SeeBattle4/src/Game.java
+++ SeeBattle4/src/Game.java
@@ -2,27 +2,25 @@
 public class Game {
     private boolean isPlayer; //Это игрок или компьютер
     Field field = new Field();
-  //  Player player = new Player();
 
     public void run() {
         System.out.println("Начали игру");
- //       field.player.getName();
+        field.player.getName();
         field.init();
         field.show();
         isPlayer = true;
-//        int choice = field.player.chooseAlignment();
-//        switch (choice) {
-//            case 1:
-//                field.setShipAuto(isPlayer);
-//                break;
-//            case 2:
-//                field.setShipManual();
-//                break;
-//            default:
-//                field.setShipAuto(isPlayer);
-//                break;
-//        }
-        field.setShipAuto(isPlayer); // комеентировать, когда открываю основной код
+        int choice = field.player.chooseAlignment();
+        switch (choice) {
+            case 1:
+                field.setShipAuto(isPlayer);
+                break;
+            case 2:
+                field.setShipManual();
+                break;
+            default:
+                field.setShipAuto(isPlayer);
+                break;
+        }
         field.setShipAuto(!isPlayer);
         do {
             field.show();
